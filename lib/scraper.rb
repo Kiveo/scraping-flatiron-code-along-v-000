db85c903caa6308f9770d4f3e0edda82d028598b
Maverick Tesini <mavericktesini@gmail.com>
2018-03-23 19:14:37 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,6 +22,7 @@ class Scraper
   end
 # doc.css(".post").first.css("h2").text
 # doc.css(".post").first.css(".date").text
+# doc.css(".post").first.css("p").text
 end #end of class
 
 Scraper.new.get_page

9438c67c884db60cb78a76e869fd3d68a7fa3813
Maverick Tesini <mavericktesini@gmail.com>
2018-03-23 19:06:23 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -16,6 +16,11 @@ class Scraper
     end
   end
   
+  def get_page
+    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/site-for-scraping/courses") )
+    
+  end
+
 end
 
 

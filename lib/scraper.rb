eda4df2b2da6c1e1281d68ca91e1218008f1dc4b
Maverick Tesini <mavericktesini@gmail.com>
2018-03-23 19:25:03 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,11 +21,11 @@ class Scraper
   end
   def get_courses
     self.get_page.css(".post")
-    doc.css(".post").each do |post|
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
-    end
+    # doc.css(".post").each do |post|
+    #   course.title = post.css("h2").text
+    #   course.schedule = post.css(".date").text
+    #   course.description = post.css("p").text
+    # end
   end
 
 end #EOC

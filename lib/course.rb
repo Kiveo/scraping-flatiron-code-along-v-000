41873ca5aa25cc4940198489a49d93ed2839df76
Maverick Tesini <mavericktesini@gmail.com>
2018-03-23 18:57:49 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,9 +1,20 @@
 
 class Course
   attr_accessor :title, :schedule, :description
+  attr_accessor :all
 
-  def initialize
+  @@all = []
+
+  def initialize(title, schedule, description)
     @title = title
+    @schedule = schedule
+    @description = description
+
+    @@all << self
+  end
+
+  def all
+    @@all
   end
 
 end
